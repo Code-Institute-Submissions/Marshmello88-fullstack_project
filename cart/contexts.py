7b993a13f89f4b11747411f209d386992f0f3ad6
Marshmello88 <m.neibauere@gmail.com>
2020-10-18 09:59:45 +0000

    added cart functionality

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,29 +1,34 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from catalog.models import Product
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
 
-    if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
-    else:
-        delivery = 0
-        free_delivery_delta = 0
-    
-    grand_total = delivery + total
-    
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+
     context = {
         'cart_items': cart_items,
         'total': total,
         'product_count': product_count,
-        'delivery': delivery,
-        'free_delivery_delta': free_delivery_delta,
-        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
-        'grand_total': grand_total,
+        #'delivery': delivery,
+        #'free_delivery_delta': free_delivery_delta,
+       # 'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        #'grand_total': grand_total,
     }
 
     return context
--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -159,5 +159,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# delete later?
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
