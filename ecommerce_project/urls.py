964b2f0372e2ac246ea60382393d0f76f29cf766
Marshmello88 <m.neibauere@gmail.com>
2020-10-28 18:16:14 +0000

    responsive

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('cart/', include('cart.urls')), #NEW
+    path('cart/', include('cart.urls')), 
     path('', include('home.urls')),
     path('products/', include('catalog.urls')),
     path('checkout/', include('checkout.urls')),
